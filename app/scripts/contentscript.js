3f06018fdf314332809049209c161619622f7477
Rab Nawaz <rnawaz02@gmail.com>
2020-08-21 08:16:43 +0500

    update

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -31,7 +31,127 @@ window.addEventListener("load", function () {
                 }
             }
         }
+
+
+
+        let glParticipant = document.getElementsByClassName('gl-Participant');
+        for (var i = 0; i < glParticipant.length; i++) {
+            glParticipant[i].removeEventListener('click', mysecondfuncParticipant)
+            glParticipant[i].addEventListener('click', mysecondfuncParticipant)
+            if (i >= 2) {
+                break;
+            }
+        }
+        let srbParticipantLabelCentered = document.getElementsByClassName('srb-ParticipantLabelCentered');
+        for (var i = 0; i < srbParticipantLabelCentered.length; i++) {
+            srbParticipantLabelCentered[i].removeEventListener('click', mysecondfuncParticipantLabelCentered)
+            srbParticipantLabelCentered[i].addEventListener('click', mysecondfuncParticipantLabelCentered)
+            if (i >= 0) {
+                break;
+            }
+        }
+        /*
+        let glMarket = document.getElementsByClassName('gl-Market');
+        let glMarketGeneral = document.getElementsByClassName('gl-Market_General');
+        let glMarketGeneralcolumnheader = document.getElementsByClassName('gl-Market_General-columnheader');
+        */
+        let glMarketGeneralpwidth375 = document.getElementsByClassName('gl-Market_General-pwidth37-5');
+        for (var i = 0; i < glMarketGeneralpwidth375.length; i++) {
+            let containers = glMarketGeneralpwidth375[i].childNodes;
+            for (var j = 1; j < containers.length; j++) {
+                containers[j].removeEventListener('click', mysecondfunccontainers)
+                containers[j].addEventListener('click', mysecondfunccontainers)
+                if (j >= 1) {
+                    break;
+                }
+            }
+            if (i >= 1) {
+                break;
+            }
+        }
     });
+    function mysecondfunccontainers(event) {
+        console.log('mysecondfunccontainers');
+        secondfunc();
+    }
+    function mysecondfuncParticipantLabelCentered(event) {
+        console.log('mysecondfuncParticipantLabelCentered');
+        secondfunc();
+    }
+    function mysecondfuncParticipant(event) {
+        console.log('mysecondfuncParticipant');
+        secondfunc();
+    }
+    function secondfunc() {
+        let EventHeader_Label = document.getElementsByClassName('sph-EventHeader_Label');
+        console.log(EventHeader_Label.length);
+        console.log(EventHeader_Label[0].innerText);
+        var html = `
+        <div class="popwrapper"><div style="height:34px; background:black"><span id="mysecondModalClose" class="close">&times;</span></div>
+            <div class="bss-NormalBetItem bss-NormalBetItem_HasStake ">
+                <div class="bss-NormalBetItem_Wrapper ">
+                    <div class="bss-NormalBetItem_Content ">
+                        <div class="bss-NormalBetItem_ContentWrapper ">
+                            <div class="bss-NormalBetItem_Details">
+                                <div class="bss-NormalBetItem_TopSection">
+                                    <div class="bss-NormalBetItem_TitleAndMarket">
+                                        <div class="bss-NormalBetItem_Title " style="">PSG in 90 mins</div>
+                                        <div class="bss-NormalBetItem_Market ">Method of Victory</div>
+                                    </div>
+                                    <div class="bss-NormalBetItem_OddsContainer "><span
+                                            class="bsc-OddsDropdownLabel bs-OddsLabel bsc-OddsDropdownLabel-disabled "><span>3.25</span></span>
+                                    </div>
+                                </div>
+                                <div class="bss-NormalBetItem_BottomSection">
+                                    <div class="bss-NormalBetItem_FixtureDescription "> ${EventHeader_Label[0].innerText}</div>
+                                    <!---->
+                                </div>
+                                <div class="bss-NormalBetItem_CheckboxContainer"></div>
+                            </div>
+                            <div class="bss-StakeBox bss-StakeBox_MouseMode bss-StakeBox_HasReturns ">
+                                <div class="bss-StakeBox_StakeAndReturn ">
+                                    <div class="bss-StakeBox_StakeInput ">
+                            
+                            
+                                    </div>
+                                </div>
+                                <div class="bss-StakeBox_TotalStake ">
+                                    <div class="bss-StakeBox_TotalStake-label ">Stake</div>
+                                    <div class="bss-StakeBox_TotalStake-value ">$0.00</div>
+                                </div>
+                            </div>
+                        </div>
+                   </div>
+                    </div>
+                    <div class="bss-PlaceBetButton ">
+                        <div class="bss-PlaceBetButton_Wrapper">
+                            <div class="bss-PlaceBetButton_TopRow">
+                                <div class="bss-PlaceBetButton_Text ">Place Bet</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    `;
+        let mySecondModal = document.getElementById('mySecondModal');
+        if (mySecondModal) {
+            //document.getElementById('myModalteam').innerText=teamString;
+            //document.getElementById('myModalvalue').innerText=value;
+            mySecondModal.style.display = "block";
+        } else {
+            mySecondModal = document.createElement('div');
+            mySecondModal.setAttribute('id', "mySecondModal");
+            mySecondModal.setAttribute('class', "modal");
+            mySecondModal.innerHTML = html;
+            mySecondModal.style.display = "block";
+            document.body.appendChild(mySecondModal);
+        }
+        document.getElementById('mysecondModalClose').onclick = function(){
+             mySecondModal.style.display = "none";
+        }
+
+
+
+    }
     function whichChild(elem) {
         var i = 0;
         while ((elem = elem.previousSibling) != null) ++i;
@@ -54,7 +174,7 @@ window.addEventListener("load", function () {
         }
         if (teams.length > 0) {
             let teamString = "";
-            let teamsOgInterest = teams[index].getElementsByClassName("rcl-ParticipantFixtureDetails_TeamWrapper ")            
+            let teamsOgInterest = teams[index].getElementsByClassName("rcl-ParticipantFixtureDetails_TeamWrapper ")
             for (var k = 0; k < teamsOgInterest.length; k++) {
                 if (teamString === "") {
                     teamString = teamsOgInterest[k].innerText;
@@ -65,23 +185,23 @@ window.addEventListener("load", function () {
             //let msg = teamString + "\n" + value;
             //alert(msg);
             let myModeldiv = document.getElementById('myModal');
-            if(myModeldiv){
-                document.getElementById('myModalteam').innerText=teamString;
-                document.getElementById('myModalvalue').innerText=value;
+            if (myModeldiv) {
+                document.getElementById('myModalteam').innerText = teamString;
+                document.getElementById('myModalvalue').innerText = value;
                 myModeldiv.style.display = "block";
-            }else{
+            } else {
                 myModeldiv = document.createElement('div');
                 myModeldiv.setAttribute('id', "myModal");
-                myModeldiv.setAttribute('class', "modal");   
+                myModeldiv.setAttribute('class', "modal");
                 myModeldiv.innerHTML = '<div class="modal-content">'
-                        +'<span id="myModalClose" class="close">&times;</span>'
-                            +'<p id="myModalteam">'+teamString+'</p>'
-                            +'<p id="myModalvalue">'+value+'</p>'
-                        +'</div>'
+                    + '<span id="myModalClose" class="close">&times;</span>'
+                    + '<p id="myModalteam">' + teamString + '</p>'
+                    + '<p id="myModalvalue">' + value + '</p>'
+                    + '</div>'
                 myModeldiv.style.display = "block";
                 document.body.appendChild(myModeldiv);
             }
-            document.getElementById('myModalClose').onclick = function(){
+            document.getElementById('myModalClose').onclick = function () {
                 myModeldiv.style.display = "none";
             }
 
